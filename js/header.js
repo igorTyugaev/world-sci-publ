3e75bd94808a887b0b7fddf261abbf61061074f7
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-02-17 10:11:55 +0500

    Add animation for menu

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
 _navToggle = document.getElementById("navToggle")
+_navBurger = document.getElementById("navBurger")
 _navBar = document.getElementById("navBar")
 
 _navToggle.addEventListener("click", () => {
-    _navToggle.classList.toggle("burger--close")
+    // _navToggle.classList.toggle("burger--close")
+    _navBurger.classList.toggle("burger-active")
     _navBar.classList.toggle("nav--show")
 })
