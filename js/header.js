74fd4889289364ee7beeebcad50c637ff17fa41f
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-02-19 14:19:12 +0500

    Fixed menu

--- js/header.js
+++ js/header.js
@@ -2,8 +2,11 @@ _navToggle = document.getElementById("navToggle")
 _navBurger = document.getElementById("navBurger")
 _navBar = document.getElementById("navBar")
 
+_menuTitle = _navToggle.querySelector(".menu__title")
+
 _navToggle.addEventListener("click", () => {
     // _navToggle.classList.toggle("burger--close")
     _navBurger.classList.toggle("burger-active")
     _navBar.classList.toggle("nav--show")
+    _menuTitle.classList.toggle("menu__title--show")
 })
