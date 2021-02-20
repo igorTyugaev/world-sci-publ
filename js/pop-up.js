d14e65f99d2f60414a4f3fa7a00e2eb90dc50062
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-02-20 23:24:13 +0500

    Fixed close btn on last pop-up

--- js/pop-up.js
+++ js/pop-up.js
@@ -8,6 +8,14 @@ function initPopUp(button) {
     const _popUp = document.getElementById(idPopUp);
     const _closeButton = _popUp.querySelector('.pop-up__close');
 
+    const _closeBtn = _popUp.querySelector('.pop-up__button--close');
+
+    if (_closeBtn) {
+        console.debug("_closeBtn");
+        _closeBtn.addEventListener('click', () => {
+            closePopUp(_popUp, duration);
+        });
+    }
 
     button.addEventListener('click', (e) => {
         // e.preventDefault();
