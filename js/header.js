e008ba188c5016664f5fb88eeaafc70626731ebc
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-02-21 14:24:02 +0500

    Add scroll for menu items

--- js/header.js
+++ js/header.js
@@ -6,25 +6,39 @@ _header = document.getElementById("header")
 _menuTitle = _navToggle.querySelector(".menu__title")
 
 _navToggle.addEventListener("click", () => {
-    // _navToggle.classList.toggle("burger--close")
-    _navBurger.classList.toggle("burger-active")
-    _navBar.classList.toggle("nav--show")
-    _menuTitle.classList.toggle("menu__title--show")
+    toggleMenu();
 })
+
+function toggleMenu() {
+    // _navToggle.classList.toggle("burger--close");
+    _navBurger.classList.toggle("burger-active");
+    _navBar.classList.toggle("nav--show");
+    _menuTitle.classList.toggle("menu__title--show");
+}
 
 
 let scrollObject = {};
 window.onscroll = getScrollPosition;
 
-function getScrollPosition(){
+function getScrollPosition() {
     scrollObject = {
         x: window.pageXOffset,
         y: window.pageYOffset
     }
 
-    if(scrollObject.y > 180) {
+    if (scrollObject.y > 180) {
         _header.classList.add("header--show")
     } else {
         _header.classList.remove("header--show")
     }
 }
+
+const buttons = document.querySelectorAll('[data-scroll]');
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        toggleMenu();
+        const anchor = button.getAttribute("data-scroll")
+        const anchorElement = document.querySelector(anchor);
+        anchorElement.scrollIntoView();
+    });
+});
