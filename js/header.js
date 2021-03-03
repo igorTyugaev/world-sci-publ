419518b7784d0af6636c3baeffb2a60a00fb424b
Igor Tyugaev <igor.tyugaev@gmail.com>
2021-03-03 17:37:08 +0500

    Add mobile baner

--- js/header.js
+++ js/header.js
@@ -1,13 +1,31 @@
-_navToggle = document.getElementById("navToggle")
-_navBurger = document.getElementById("navBurger")
-_navBar = document.getElementById("navBar")
-_header = document.getElementById("header")
+_navToggle = document.getElementById("navToggle");
+_navBurger = document.getElementById("navBurger");
+_navBar = document.getElementById("navBar");
+_header = document.getElementById("header");
+_callBtn = document.getElementById("header-call-btn");
 
-_menuTitle = _navToggle.querySelector(".menu__title")
+_menuTitle = _navToggle.querySelector(".menu__title");
 
 _navToggle.addEventListener("click", () => {
     toggleMenu();
 })
+
+_callBtn.addEventListener('click', () => {
+
+})
+
+function triggerGoal() {
+    /* этот код создает цель в метрике */
+    console.log("Сработала метрика: call_1");
+    if (localStorage.getItem('successGoals') === null) {
+        localStorage.setItem('successGoals', "call_1");
+
+        if (typeof yaCounter50181778 !== 'undefined') {
+            yaCounter50181778.reachGoal("call_1");
+            console.log("reachGoal: call_1");
+        }
+    }
+}
 
 function toggleMenu() {
     _navBurger.classList.toggle("burger-active");
@@ -29,8 +47,8 @@ scrollItems.forEach((item) => {
         const anchor = item.getAttribute("data-scroll")
         const anchorElement = document.querySelector(anchor);
         let yOffset = 0.99;
-            if (anchor === "#cost" || anchor === "#quiz-0" || anchor === "#promo")
-                yOffset = 0.96;
+        if (anchor === "#cost" || anchor === "#quiz-0" || anchor === "#promo")
+            yOffset = 0.96;
         const y = (anchorElement.getBoundingClientRect().top + window.pageYOffset) * yOffset;
         window.scrollTo({top: y, behavior: 'smooth'});
     });
